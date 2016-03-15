8fdf4437fc1691a714dfe9671aff56851dc1586e
sescobb27 <s.escob.b.27@gmail.com>
2016-03-15 06:52:35 -0500

    [add] chainable promises example

---
+++ bin/chainable-promises.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let promiseNumber = new Promise((resolve, reject) => {
+  resolve(5);
+});
+
+let promiseString = new Promise((resolve, reject) => {
+  resolve("Hello World");
+});
+
+promiseNumber
+  .then((number) => {
+    console.log(number);
+    return promiseString;
+  })
+  .then((str) => {
+    console.log(str);
+  });
+
+
+let promiseError = new Promise((resolve, reject) => {
+  reject(new Error("Validation Error"));
+});
+
+promiseNumber
+  .then((number) => {
+    console.log(number);
+    return promiseError;
+  })
+  .then(() => {
+    return promiseString;
+  })
+  .then((str) => {
+    console.log(str);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+Promise.all([
+    promiseNumber,
+    promiseString
+  ])
+  .then((result) => {
+    console.log('promiseNumber: ', result[0]);
+    console.log('promiseString: ', result[1]);
+  });
